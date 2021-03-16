0294c68364a9fa7c85554abeea9b7ccb69487547
Brian Wignall <brianwignall@gmail.com>
2021-03-16 13:55:27 -0400

    Update test to new Spark API

--- src/main/scala/com/twosigma/flint/sql/function/aggregate/WeightedMeanTest.scala
+++ src/main/scala/com/twosigma/flint/sql/function/aggregate/WeightedMeanTest.scala
@@ -16,12 +16,9 @@
 
 package com.twosigma.flint.sql.function.aggregate
 
-import org.apache.spark.sql.Row
-import org.apache.spark.sql.expressions.{
-  MutableAggregationBuffer,
-  UserDefinedAggregateFunction
-}
-import org.apache.spark.sql.types._
+import org.apache.spark.sql.Encoder
+import org.apache.spark.sql.expressions.Aggregator
+import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
 
 import scala.math.{ abs, signum, sqrt }
 
@@ -37,107 +34,112 @@ import scala.math.{ abs, signum, sqrt }
  * and replaces all "n" with corresponding "SumWeight"
  */
 
-class WeightedMeanTest extends UserDefinedAggregateFunction {
-  override def inputSchema: StructType =
-    StructType(
-      StructField("value", DoubleType) :: StructField(
-        "weight",
-        DoubleType
-      ) :: Nil
-    )
+class InputSchema(val value: Double, val weight: Double)
+//class BufferSchema extends Tuple[Long, Double, Double, Double, Double]
+//class DataType extends Tuple[Double, Double, Double, Long]
 
-  override def bufferSchema: StructType =
-    StructType(
-      StructField("count", LongType) ::
-        StructField("sumWeight", DoubleType) ::
-        StructField("mean", DoubleType) ::
-        StructField("sumSquareOfDiffFromMean", DoubleType) ::
-        StructField("sumSquareOfWeights", DoubleType) :: Nil
-    )
+class BufferSchema(
+  val count: Long,
+  val sumWeight: Double,
+  val mean: Double,
+  val sumSquareOfDiffFromMean: Double,
+  val sumSquareOfWeights: Double
+)
 
-  override def dataType: DataType =
-    StructType(
-      StructField("weightedMean", DoubleType) ::
-        StructField("weightedStandardDeviation", DoubleType) ::
-        StructField("weightedTstat", DoubleType) ::
-        StructField("observationCount", LongType) :: Nil
-    )
+class DataType(
+  val weightedMean: Double,
+  val weightedStandardDeviation: Double,
+  val weightedTstat: Double,
+  val observationCount: Long
+)
 
-  override def deterministic: Boolean = true
+class WeightedMeanTest extends Aggregator[InputSchema, BufferSchema, DataType] {
 
-  override def initialize(buffer: MutableAggregationBuffer): Unit = {
-    buffer(0) = 0L
-    buffer(1) = 0.0
-    buffer(2) = 0.0
-    buffer(3) = 0.0
-    buffer(4) = 0.0
+  override def zero: BufferSchema = {
+    new BufferSchema(0L, 0.0, 0.0, 0.0, 0.0)
   }
 
-  override def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
-    val rawValue = input.getAs[Double](0)
-    val rawWeight = input.getAs[Double](1)
+  override def reduce(
+    buffer: BufferSchema,
+    input: InputSchema
+  ): BufferSchema = {
+    val rawValue = input.value
+    val rawWeight = input.weight
 
     // Move the sign from weight to value, keep weight non-negative
     val value = rawValue * signum(rawWeight)
     val weight = abs(rawWeight)
 
-    val count = buffer.getAs[Long](0)
-    val sumWeight = buffer.getAs[Double](1)
-    val mean = buffer.getAs[Double](2)
-    val sumSquareOfDiffFromMean = buffer.getAs[Double](3)
-    val sumSquareOfWeights = buffer.getAs[Double](4)
+    val count = buffer.count
+    val sumWeight = buffer.sumWeight
+    val mean = buffer.mean
+    val sumSquareOfDiffFromMean = buffer.sumSquareOfDiffFromMean
+    val sumSquareOfWeights = buffer.sumSquareOfWeights
 
     val temp = weight + sumWeight
     val delta = value - mean
     val R = delta * weight / temp
 
-    buffer(0) = count + 1
-    buffer(1) = temp
-    buffer(2) = mean + R
-    buffer(3) = sumSquareOfDiffFromMean + (sumWeight * delta * R)
-    buffer(4) = sumSquareOfWeights + (weight * weight)
+    new BufferSchema(
+      count + 1,
+      temp,
+      mean + R,
+      sumSquareOfDiffFromMean + (sumWeight * delta * R),
+      sumSquareOfWeights + (weight * weight)
+    )
   }
 
-  override def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
-    val count2 = buffer2.getAs[Long](0)
+  override def merge(
+    buffer1: BufferSchema,
+    buffer2: BufferSchema
+  ): BufferSchema = {
+    val count2 = buffer2.count
 
     if (count2 > 0) {
-      val count1 = buffer1.getAs[Long](0)
-      val sumWeight1 = buffer1.getAs[Double](1)
-      val mean1 = buffer1.getAs[Double](2)
-      val sumSquareOfDiffFromMean1 = buffer1.getAs[Double](3)
-      val sumSquareOfWeights1 = buffer1.getAs[Double](4)
+      val count1 = buffer1.count
+      val sumWeight1 = buffer1.sumWeight
+      val mean1 = buffer1.mean
+      val sumSquareOfDiffFromMean1 = buffer1.sumSquareOfDiffFromMean
+      val sumSquareOfWeights1 = buffer1.sumSquareOfWeights
 
-      val sumWeight2 = buffer2.getAs[Double](1)
-      val mean2 = buffer2.getAs[Double](2)
-      val sumSquareOfDiffFromMean2 = buffer2.getAs[Double](3)
-      val sumSquareOfWeights2 = buffer2.getAs[Double](4)
+      val sumWeight2 = buffer2.sumWeight
+      val mean2 = buffer2.mean
+      val sumSquareOfDiffFromMean2 = buffer2.sumSquareOfDiffFromMean
+      val sumSquareOfWeights2 = buffer2.sumSquareOfWeights
 
       val delta = mean2 - mean1
 
-      buffer1(0) = count1 + count2
-      buffer1(1) = sumWeight1 + sumWeight2
+      val new0 = count1 + count2
+      val new1 = sumWeight1 + sumWeight2
       // This particular way to calculate mean is chosen based on
       // http://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Parallel_algorithm
-      buffer1(2) =
+      val new2 =
         (sumWeight1 * mean1 + sumWeight2 * mean2) / (sumWeight1 + sumWeight2)
-      buffer1(3) = sumSquareOfDiffFromMean1 + sumSquareOfDiffFromMean2 +
+      val new3 = sumSquareOfDiffFromMean1 + sumSquareOfDiffFromMean2 +
         delta * delta * sumWeight1 * sumWeight2 / (sumWeight1 + sumWeight2)
-      buffer1(4) = sumSquareOfWeights1 + sumSquareOfWeights2
+      val new4 = sumSquareOfWeights1 + sumSquareOfWeights2
+
+      new BufferSchema(new0, new1, new2, new3, new4)
+    } else {
+      buffer1
     }
   }
 
-  override def evaluate(buffer: Row): Any = {
-    val count = buffer.getAs[Long](0)
-    val sumWeight = buffer.getAs[Double](1)
-    val mean = buffer.getAs[Double](2)
-    val sumSquareOfDiffFromMean = buffer.getAs[Double](3)
+  override def finish(buffer: BufferSchema): DataType = {
+    val count = buffer.count
+    val sumWeight = buffer.sumWeight
+    val mean = buffer.mean
+    val sumSquareOfDiffFromMean = buffer.sumSquareOfDiffFromMean
 
     val variance = sumSquareOfDiffFromMean / sumWeight
     val stdDev = sqrt(variance)
     val tStat = sqrt(count.toDouble) * mean / stdDev
 
-    Row(mean, stdDev, tStat, count)
+    new DataType(mean, stdDev, tStat, count)
   }
+
+  override def bufferEncoder: Encoder[BufferSchema] = ExpressionEncoder()
+
+  override def outputEncoder: Encoder[DataType] = ExpressionEncoder()
 
 }
